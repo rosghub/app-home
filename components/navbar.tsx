d75afa5c1803bd4f72f2199526f6f59e517bce3e
brandon-desk <brosini9@gmail.com>
2021-09-09 17:38:07 -0400

    navbar css

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const navbar = () => {
     return (
-        <nav className="navbar is-spaced has-background-warning" aria-label="main navigation">
+        <nav className="navbar is-spaced has-background-light" aria-label="main navigation">
             <div className="navbar-brand">
                 <a className="navbar-item" href="https://rosghub.xyz">
                     <span className="title is-5">rosghub.xyz</span>
@@ -12,7 +14,7 @@ const navbar = () => {
             <div className="navbar-end mr-4" style={{ display: 'flex' }}>
                 <a href="https://github.com" target="_blank">
                     <span className="icon has-text-black">
-                        <i className="fab fa-github fa-lg"></i>
+                        <FontAwesomeIcon icon={faGithub} size="lg" />
                     </span>
                 </a>
             </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/styles.css';
 import '../styles/topbar.css';
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
