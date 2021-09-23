c28709bc6e33e7d57d30ba7d6542148e4ad421c2
Brandon <brosini9@gmail.com>
2021-09-23 01:50:51 -0400

    tech filter selection

--- components/FilterTech.tsx
+++ components/FilterTech.tsx
@@ -6,21 +6,44 @@ interface AggregateTechProps {
     apps: App[],
 }
 
-const FilterTech: FC<AggregateTechProps> = (props): JSX.Element => {
+function getTags(apps: App[]): string[] {
     const unique: Array<string> = ['All'];
-
-    props.apps.forEach(app => {
+    apps.forEach(app => {
         unique.push(...app.tech.filter(t => unique.indexOf(t) == -1))
     });
+    return unique;
+}
 
+const FilterTech: FC<AggregateTechProps> = (props): JSX.Element => {
     const [selected, setSelected] = useState<Array<string>>(['All']);
 
+    const tags = getTags(props.apps);
+
     const clickHandler = (e: React.MouseEvent<HTMLElement>) => {
-        console.log('Tag clicked: ' + e.currentTarget.textContent);
+        const tag = e.currentTarget.textContent;
+        if (tag == null)
+            return;
+
+        if (tag == 'All') {
+            // Clear selection
+            setSelected(['All']);
+        }
+        else if (selected.includes(tag)) {
+            // Unselect
+            const selection = selected.filter(t => t != tag);
+            setSelected(selection.length > 0 ? selection : ['All']);
+        }
+        else {
+            // Select
+            if (selected.length == 1 && selected[0] == 'All')
+                selected.pop();
+
+            setSelected([...selected, tag]);
+        }
     }
 
     return (<>
-        {unique.map((t, i) => {
+        {tags.map((t, i) => {
             let classes = `tag is-rounded is-normal my-1 mr-2 ${styles.tag}`;
             if (selected.includes(t))
                 classes += ' is-info';
